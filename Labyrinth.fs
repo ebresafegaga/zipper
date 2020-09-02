4b708f86b8d95de2408746bcdcbc70cd2c519d8b
ebresafegaga <ebresafegaga@gmail.com>
2020-09-02 01:27:35 +0100

    Added an implementation for Labytinth/.fs

--- Labyrinth.fs
+++ Labyrinth.fs
@@ -4,4 +4,120 @@ module Labyrinth
 // https://en.wikibooks.org/wiki/Haskell/Zippers
 // 
 
+type Node<'a> = 
+    | DeadEnd of 'a 
+    | Passage of 'a * Node<'a> 
+    | Fork of 'a * Node<'a> * Node<'a> 
+    | Gold 
 
+let get = 
+    function 
+    | DeadEnd a | Passage (a, _) 
+    | Fork (a, _, _) -> a
+
+let put a = 
+    function
+    | DeadEnd _ -> DeadEnd a 
+    | Passage (_, node) -> Passage (a, node)
+    | Fork (_, n1, n2) -> Fork (a, n1, n2)
+
+// let gold = failwith ""
+
+let lab = 
+    Fork ((2, 4), 
+        Fork ((0, 2),
+            DeadEnd (1, 2), 
+            DeadEnd (2, 0)), 
+        Passage ((4, 2), 
+            Fork ((1, 2), 
+                Passage ((2, 3), Gold), 
+                DeadEnd (2, 1))))
+
+
+// let turnRight = 
+//     function 
+//     | Fork (_, l, r) -> Some r 
+//     | _ -> None 
+
+
+type Branch = 
+    | KeepStraightOn 
+    | TurnLeft 
+    | TurnRight 
+
+type Thread = Branch list 
+
+let turnRight (t: Thread) : Thread = t @ [TurnRight]
+
+let rec retreive (t: Thread) node = 
+    match t, node with 
+    | [], _ -> get node
+    | _, DeadEnd a -> assert false; a // this shouldn't happen 
+    | TurnLeft :: bs, Fork (_, l, _) -> retreive bs l 
+    | TurnRight :: bs, Fork (_, _, r) -> retreive bs r
+    | KeepStraightOn :: bs, Passage (_, d) -> retreive bs d
+    | (TurnLeft | TurnRight) :: bs, Passage (_, d) -> 
+        assert false; // this shouldn't happen 
+        retreive bs d 
+    | KeepStraightOn :: bs, Fork (_, l, _) -> 
+        assert false; // This shouldn't happen
+        retreive bs l
+
+let rec update f (t: Thread) node = 
+    match t, node with 
+    | [], _ -> put (f (get node)) node
+    | TurnLeft :: bs, Fork (a, l, r) -> Fork (a, update f bs l, r) 
+    | TurnRight :: bs, Fork (a, l, r) -> Fork (a, l, update f bs r)
+    | KeepStraightOn :: bs, Passage (a, s) -> Passage (a, update f bs s)
+    | _, DeadEnd a -> failwith "update: _, DeadEnd a" 
+    | (TurnLeft | TurnRight) :: bs, Passage (_, d) -> 
+        failwith "update: (TurnLeft | TurnRight) :: bs, Passage (_, d)"
+    | KeepStraightOn :: bs, Fork (_, l, _) -> 
+        failwith "update: KeepStraightOn :: bs, Fork (_, l, _)"
+    
+module Ariadne = 
+
+    type Branch<'a> = 
+        | KeepStraightOn of 'a 
+        | TurnLeft of 'a * Node<'a> 
+        | TurnRight of 'a * Node<'a> 
+    
+    type Thread<'a> = Branch<'a> list 
+
+    type Zipper<'a> = Thread<'a> * Node<'a> 
+
+    let turnRight (zipper: Zipper<'a>) : Zipper<'a> option =
+        match zipper with 
+        | t, Fork (x, l, r) -> Some (TurnRight (x, l) :: t, r)
+        | _ -> None
+    
+    let turnLeft (zipper: Zipper<'a>) : Zipper<'a> option =
+        match zipper with 
+        | t, Fork (x, l, r) -> Some (TurnLeft (x, r) :: t, l)
+        | _ -> None
+    
+    let keepStraightOn (zipper: Zipper<'a>) : Zipper<'a> option =
+        match zipper with 
+        | t, Passage (x, n) -> Some (KeepStraightOn x :: t, n)
+        | _ -> None
+    
+    let back (zipper: Zipper<'a>) : Zipper<'a> option = 
+        match zipper with 
+        | [], _ -> None 
+        | KeepStraightOn x :: t, n -> Some (t, Passage (x, n))
+        | TurnLeft (x, r) :: t, l -> Some (t, Fork (x, l, r)) 
+        | TurnRight (x, l) :: t, r -> Some (t, Fork (x, l ,r))
+    
+    let get (x: Zipper<'a>) =
+        match x with 
+        | _, node -> get node
+
+    let put (x: Zipper<'a>) a : Zipper<'a> = 
+        match x with 
+        | thread, _ -> thread, a
+
+    let update f (x: Zipper<'a>) : Zipper<'a> =
+        match x with 
+        | thread, Fork (x, l, r) -> thread, Fork (f x, l, r)
+        | thread, Passage (x, d) -> thread, Passage (f x, d)
+        | thread, DeadEnd x -> thread, DeadEnd (f x)
--- Program.fs
+++ Program.fs
@@ -1,49 +1,7 @@
 ﻿
 
-// notRev x ++ notRev y == notRev (y ++ x)
 
-let rec zipWith f xs ys =
-    match xs, ys with 
-    | x :: xs, y :: ys -> f x y :: zipWith f xs ys
-    | [], [] -> [] // same length, but done
-    | _ -> [] // not the same length 
- 
-let rec accumulate f vector = 
-    match vector with 
-    | [] -> []
-    | row :: rest -> 
-        List.reduce f (List.head row :: List.map List.head rest)
-        :: accumulate f (List.tail row :: List.map List.tail rest)
-
-type Lzy<'a> = Lzy of (unit -> 'a)
-
-let frc (Lzy f) = f ()
-
-let ($) = (<|)
-
-let a = Lzy $ fun () -> 10
-
-let p x = fun y -> x 
-
-let (<*>) f a = 
-    fun x -> f x (a x)
-
-let a' l = 
-    zipWith (<=) l l.[1..] 
-    |> List.forall id
-
-let a'' l =
-    l 
-    |> List.fold (fun (v, b) a -> (a, v <= a && b)) (0, true)
-    |> snd
-
-let rec qsort l =
-    match l with 
-    | x :: xs -> 
-        let left = [ for i in xs do if i <= x then yield i ]
-        let right = [ for i in xs do if i > x then yield i ]
-        (qsort left) @ [x] @ (qsort right)
-    | [] -> []
+// Write a complete game based on Theseus' labyrinth
 
 [<EntryPoint>]
 let main argv = 0 
